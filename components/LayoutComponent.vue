12594b4a30877dcb5a2fa960958bfa7cae5140c2
André Nolasco <nolasco3d@gmail.com>
2022-02-21 22:57:33 -0300

    Add product cards component

--- components/LayoutComponent.vue
+++ components/LayoutComponent.vue
@@ -2,7 +2,13 @@
   <div class="">
     <div class="navbar">
       <div class="navbar--container">
-        <h4>Container</h4>
+        <div class="navbar--logo">
+          <!-- <img src="" alt=""> -->
+        </div>
+        <div class="navbar--menus">
+          <div class="links"></div>
+          <div class="social-media"></div>
+        </div>
       </div>
     </div>
     <slot name="content" />
@@ -15,10 +21,20 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .navbar {
   @apply bg-gray-50 top-0 absolute w-full h-12
     filter drop-shadow-sm;
+
+  &--container {
+    @apply container mx-auto h-full border border-black flex;
+  }
+  &--logo {
+    @apply border border-blue-300 h-full w-12;
+  }
+  &--menus {
+    @apply border border-green-300 w-full;
+  }
 }
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,69 @@
 <template>
-  <div>
-    <LayoutComponent>
-      <template #content>
-        <BannerHero />
-      </template>
-    </LayoutComponent>
-  </div>
+  <LayoutComponent>
+    <template #content>
+      <BannerHero />
+      <div class="product--grid">
+        <template v-for="(product, index) in products">
+          <div :key="`product-${index}`" class="product--card">
+            <h4>{{ product.name }}</h4>
+            <p>{{ product.desc }}</p>
+            <span>{{ product.price }}</span>
+            <button>Click</button>
+          </div>
+        </template>
+      </div>
+    </template>
+  </LayoutComponent>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      products: [
+        {
+          name: 'Prod 1',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus non vitae nulla?',
+          price: 12392,
+          img: 'none',
+        },
+        {
+          name: 'Prod 1',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus non vitae nulla?',
+          price: 12392,
+          img: 'none',
+        },
+        {
+          name: 'Prod 1',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus non vitae nulla?',
+          price: 12392,
+          img: 'none',
+        },
+        {
+          name: 'Prod 1',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus non vitae nulla?',
+          price: 12392,
+          img: 'none',
+        },
+      ],
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.product {
+  &--grid {
+    @apply border border-gray-200 grid grid-cols-3 gap-5 p-5;
+    margin-top: -120px;
+    padding-bottom: -120px;
+  }
+  &--card {
+    @apply
+      border border-gray-200 bg-gray-100 text-gray-600
+      rounded rounded-lg
+      p-3;
+  }
+}
+</style>
